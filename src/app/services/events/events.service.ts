71cbaad957cf550617ac5513c66379d896f22277
Ricardo Maya <rickmaya@gmail.com>
2023-10-25 16:47:39 -0500

    feature: create an event service and to get all events

--- src/app/services/events/events.service.ts
+++ src/app/services/events/events.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ApiResponse } from 'src/app/components/event-listing/apiResponse.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
+  private apiUrl = 'http://localhost:9095'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getEvents(): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(`${this.apiUrl}/api/events/`);
+  }
 }
