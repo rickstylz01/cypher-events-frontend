0cc93ff76c29a2da620ba23d442b813b730026a2
Ricardo Maya <rickmaya@gmail.com>
2023-10-25 09:58:37 -0500

    feature: add FormsModule directives in input elements and define properties in registration component

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { error } from 'console';
+import { response } from 'express';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private userService: UserService) { }
+
+  register() {
+    const userData = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.userService.registerUser(userData).subscribe(
+      response => {
+        console.log('Registreation successfull', response);
+      },
+      error => {
+        console.error('Registration failed', error);
+      }
+    )
+  }
 }
