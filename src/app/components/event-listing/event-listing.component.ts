0740ca93959dc54cf434c6950b9e32a4846774cc
Ricardo Maya <rickmaya@gmail.com>
2023-10-25 14:45:20 -0500

    modify: add event-listing component in its parent homepage component

---
+++ src/app/components/event-listing/event-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-listing',
+  templateUrl: './event-listing.component.html',
+  styleUrls: ['./event-listing.component.css']
+})
+export class EventListingComponent {
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  // create a property that represents the authentication state.
+  //when the user logs in
+  // set the property to 'true'
+
+  // call setAuthenticated() method after successful login to set isAuthenticated to true
+
+  // Implement logout logic 
+  // I.E. clear user data from localStorage, etc.
 }
