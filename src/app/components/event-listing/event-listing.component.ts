c09738b4ed928f762eff0b3c39245aaeeb3ac47d
Ricardo Maya <rickmaya@gmail.com>
2023-10-25 16:48:19 -0500

    create interfaces to account for DTOs being used in the backend

--- src/app/components/event-listing/event-listing.component.ts
+++ src/app/components/event-listing/event-listing.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from 'src/app/services/events/events.service';
+import { EventDTO } from './responseModel.model';
 
 @Component({
   selector: 'app-event-listing',
   templateUrl: './event-listing.component.html',
   styleUrls: ['./event-listing.component.css']
 })
-export class EventListingComponent {
+export class EventListingComponent implements OnInit {
+  events: EventDTO[] = [];
 
+  constructor(private eventsService: EventsService) {}
+
+  ngOnInit(): void {
+    this.eventsService.getEvents().subscribe(response => {
+      this.events = response.data;
+    })
+  }
 }
