5fa22dd9b9e0ab4e7f989a5356bb58197814febc
Ricardo Maya <rickmaya@gmail.com>
2023-10-25 13:32:15 -0500

    modify: add router-outlet to app html and modify routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
     component: RegistrationComponent
+  },
+  {
+    path: 'home',
+    component: HomepageComponent
   }
 ];
 
