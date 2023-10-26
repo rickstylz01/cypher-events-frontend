b5b38f397895bedb5862917d5cc4e1655e6936d6
Ricardo Maya <rickmaya@gmail.com>
2023-10-25 22:21:45 -0500

    feature: implement OnInit in eventDetailsComponent to retrieve data for an event

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from 'src/app/services/events/events.service';
+import { EventDTO } from '../event-listing/responseModel.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.css']
 })
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit {
+  eventId: number = 0; // Initialze with a default value
+  eventDetails: EventDTO | undefined; // Initialize as undefined
 
+  constructor(private route: ActivatedRoute, private eventsService: EventsService, ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.eventId = +params['id']; 
+
+      this.eventsService.getEventById(this.eventId).subscribe((response: EventDTO) => {
+        this.eventDetails = response;
+        console.log("Event Data: ", this.eventDetails);
+      },
+      error => {
+        console.error("Error fetching event dtails: ", error)
+      })
+    })
+  }
 }
--- src/app/components/event-listing/apiResponse.model.ts
+++
@@ -1,4 +0,0 @@
-export interface ApiResponse {
-  data: any[];
-  message: string;
-}
