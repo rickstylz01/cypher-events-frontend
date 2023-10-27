f93dc73549ed79f882c4c34f9c27d11d4c92ba6b
Ricardo Maya <rickmaya@gmail.com>
2023-10-26 23:18:09 -0500

    create auth methods to save and retrieve user id and authtoken

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:9095'
+  private apiUrl = 'http://localhost:9095';
+  private authTokenKey = 'authToken';
+  private userIdKey = 'userId';
 
   constructor(private http: HttpClient) { }
 
@@ -14,6 +17,33 @@ export class UserService {
   }
 
   loginUser(userData: any) {
-    return this.http.post(`${this.apiUrl}/auth/users/login/`, userData);
+    return this.http.post(`${this.apiUrl}/auth/users/login/`, userData)
+    .pipe(
+      tap((response: any) => {
+        this.saveAuthToken(response.token);
+        this.saveUserId(response.userId);
+      })
+    );
+  }
+
+  // Save the auth token to localStorage
+  private saveAuthToken(token: string): void {
+    localStorage.setItem(this.authTokenKey, token);
+  }
+
+  private saveUserId(userId: any): void {
+    localStorage.setItem(this.userIdKey, userId);
+  }
+
+  getAuthToken(): string | null {
+    return localStorage.getItem(this.authTokenKey);
+  }
+
+  getUserId(): any | null {
+    return localStorage.getItem(this.userIdKey)
+  }
+
+  logoutUser(): void {
+    localStorage.removeItem(this.authTokenKey);
   }
 }
